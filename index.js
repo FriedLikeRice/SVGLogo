050b631d8cd79f4ccb148a6a44c7896b7a440db4
FriedLikeRice <jacobleeferraro@gmail.com>
2024-03-03 00:39:12 -0800

    got to generate svg

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
-const promptUser = require('./lib/utils/userInput');
-const writeToFile = require('./lib/utils/fileWriter');
-const Triangle = require('./lib/shapes/Triangle');
-const Circle = require('./lib/shapes/Circle');
-const Square = require('./lib/shapes/Square');
+const { Triangle, Circle, Square } = require('./lib/shapes');
+const promptUser = require('./lib/userInput');
+const writeToFile = require('./lib/fileWriter');
 
 const run = async () => {
-  const userInput = await promptUser();
+  try {
+    const userInput = await promptUser();
 
-  let shape;
-  switch (userInput.shape) {
-    case 'triangle':
-      shape = new Triangle();
-      break;
-    case 'circle':
-      shape = new Circle();
-      break;
-    case 'square':
-      shape = new Square();
-      break;
-    default:
-      console.error('Invalid shape selected.');
-      return;
+    let shape;
+    switch (userInput.shape.toLowerCase()) {
+      case 'triangle':
+        shape = new Triangle();
+        break;
+      case 'circle':
+        shape = new Circle();
+        break;
+      case 'square':
+        shape = new Square();
+        break;
+      default:
+        console.error('Invalid shape selected.');
+        return;
+    }
+
+    shape.setColor(userInput.shapeColor);
+
+    const svgCode = shape.render();
+    writeToFile('logo.svg', svgCode);
+
+    console.log('Generated logo.svg');
+  } catch (error) {
+    console.error('An error occurred:', error.message);
   }
-
-  shape.setColor(userInput.shapeColor);
-
-  const svgCode = shape.render();
-  writeToFile('logo.svg', svgCode);
-
-  console.log('Generated logo.svg');
 };
 
 run();
